6cddc7085d9413835f582fddb613979cd86ba14b
farhan20044 <farhan.jameel@hazelsoft.org>
2025-07-14 13:46:08 +0500

    inital-commit

---
+++ Volton/Controllers/ApplicationController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Volton.Controllers
+{
+    public class ApplicationController : Controller
+    {
+        public IActionResult Page1()
+        {
+            return View();
+        }
+        public IActionResult Page2()
+        {
+            return View();
+        }
+        public IActionResult Page3()
+        {
+            return View();
+        }
+        public IActionResult Page4()
+        {
+            return View();
+        }
+    }
+}
